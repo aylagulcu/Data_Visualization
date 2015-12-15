a3f9ded985734cd479c9e25a9e4cd616a05461c5
aylagulcu <aylagulcu@gmail.com>
2015-12-15 13:48:57 +0200

    15 dec

--- DataManipulation.py
+++ DataManipulation.py
@@ -21,28 +21,71 @@ print('Length when empty cells removed: '+ str(len(data_MortalityRate_NoMissing)
 # Inner joins yield a DataFrame that contains only rows where the value being joins exists in BOTH tables. 
 data = pd.merge(left= data_GDP, right= data_MortalityRate)
 print('Length of the new merged data: '+ str(len(data)))
+#setting variables you will be working with to numeric
+data['Year'] = pd.to_numeric(data['Year'])
+data['GDP'] = pd.to_numeric(data['GDP'])
+data['Under_five_mortality'] = pd.to_numeric(data['Under_five_mortality'])
 
 print('Number of columns in this data frame: '+ str(len(data.columns)))
 print(data.columns)
 
-# Area Column frequency distribution: value_counts ile
-print('Number of rows for each Area...:')
-countArea= data['Area'].value_counts(sort= False)
-print(countArea)
+# Area Column frequency distribution: using value_counts function
+print('Area column...:')
+print('Total number of observations for Area column...:')
+print(len(data['Area'])) # number of observations for Area column
 
-print('Percentages for each Area...:')
-perArea= data['Area'].value_counts(sort= False, normalize= True)
-print(perArea)
+countAreaDistr= data['Area'].value_counts(sort= False, dropna=False)
+
+print ('Number of Different Areas:')
+print(len(countAreaDistr))
+print('Number of observations for each Area...:')
+print(countAreaDistr)
+
+print('Percentages of observations for each Area...:')
+perAreaDistr= data['Area'].value_counts(sort= False, dropna=False, normalize= True)
+print(perAreaDistr)
 
 
-#frequency distributions:
-areaDistr= data.groupby('Area').size()
-print(areaDistr)
+# GDP Column frequency distribution: using value_counts function
+print('GDP column...:')
+print('Total number of observations for GDP column...:')
+print(len(data['GDP'])) # number of observations for Area column
+
+print ('Number of empty observations for the GDP column:')
+sub1=data[(data['GDP']>0)]
+print(len(data)-len(sub1))
+
+countGDPDistr= data['GDP'].value_counts(sort= False, dropna=False)
+
+print ('Number of Different GDP values:')
+print(len(countGDPDistr))
+print('Number of observations for each GDP...:')
+print(countGDPDistr)
+
+print('Percentages of observations for each GDP...:')
+perGDPDistr= data['GDP'].value_counts(sort= False, dropna=False, normalize= True)
+print(perGDPDistr)
 
 
+# Under_five_mortality Column frequency distribution: using value_counts function
+print('Under_five_mortality column...:')
+print('Total number of observations for Under_five_mortality column...:')
+print(len(data['Under_five_mortality'])) # number of observations for Area column
 
+print ('Number of empty observations for the Under_five_mortality column:')
+sub2=data[(data['Under_five_mortality']>0)]
+print(len(data)-len(sub2))
 
+countUnder_five_mortalityDistr= data['Under_five_mortality'].value_counts(sort= False, dropna=False)
 
+print ('Number of Different Under_five_mortality values:')
+print(len(countUnder_five_mortalityDistr))
+print('Number of observations for each Under_five_mortality...:')
+print(countUnder_five_mortalityDistr)
+
+print('Percentages of observations for each Under_five_mortality...:')
+perUnder_five_mortalityDistr= data['GDP'].value_counts(sort= False, dropna=False, normalize= True)
+print(perUnder_five_mortalityDistr)
 
 
 
